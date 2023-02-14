64364486376b2f59ff2b71cf91057bd74cd857b7
Martin Bock <mail.martinbock@gmail.com>
2023-02-14 23:42:34 +0100

    draft of the search screen

--- app/src/main/java/com/example/recipefinder/ui/search/SearchScreen.kt
+++ app/src/main/java/com/example/recipefinder/ui/search/SearchScreen.kt
@@ -1,21 +1,73 @@
 package com.example.recipefinder.ui.search
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.recipefinder.data.Datasource
 import com.example.recipefinder.model.Recipepreview
 import com.example.recipefinder.ui.components.RecipeCard
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen() {
-    RecipecardList(recipecardList = Datasource().loadRecipes())
-}
+    var openDialog = remember { mutableStateOf(false) }
 
-@Composable
-fun RecipecardList(recipecardList: List<Recipepreview>) {
-    LazyColumn() {
-        items(recipecardList) { recipe ->
+    if (openDialog.value) {
+        AlertDialog(
+            onDismissRequest = {
+                // Dismiss the dialog when the user clicks outside the dialog or on the back
+                // button. If you want to disable that functionality, simply use an empty
+                // onDismissRequest.
+                openDialog.value = false
+            }
+        ) {
+            Surface(
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .wrapContentHeight(),
+                shape = MaterialTheme.shapes.large
+            ) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                        text = "Settings",
+                        style = MaterialTheme.typography.labelLarge
+                    )
+                    Spacer(modifier = Modifier.height(24.dp))
+                    Text(
+                        text = "Cuisine"
+                    )
+                    TextButton(
+                        onClick = {
+                            openDialog.value = false
+                        },
+                        modifier = Modifier.align(Alignment.End)
+                    ) {
+                        Text("Confirm")
+                    }
+                }
+            }
+        }
+    }
+
+    var text by rememberSaveable { mutableStateOf("") }
+
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(bottom = 16.dp)
+    ) {
+        item {
+            SearchSettings(text = text, toggleDialog = {openDialog.value = true}) {
+                text = it
+            }
+        }
+        items(Datasource().loadRecipes()) { recipe ->
             RecipeCard(recipe)
         }
     }
--- app/src/main/java/com/example/recipefinder/ui/search/SearchSettings.kt
+++ app/src/main/java/com/example/recipefinder/ui/search/SearchSettings.kt
@@ -1,32 +1,61 @@
 package com.example.recipefinder.ui.search
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchSettings(text: String, onInputChange: (String)->Unit) {
+fun SearchSettings(text: String, toggleDialog: ()->Unit, onInputChange: (String)->Unit) {
     Card(Modifier.fillMaxWidth(), shape = RoundedCornerShape(0.dp, 0.dp, 16.dp, 16.dp)) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(16.dp).fillMaxWidth(),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
             verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
             Text(text = "Search", style = MaterialTheme.typography.headlineMedium)
-            TextField(
+            OutlinedTextField(
                 value = text,
-                onValueChange = onInputChange
+                onValueChange = onInputChange,
+                label = { Text("Search for Recipes") },
+                placeholder = { Text("Search for Recipes") },
+                singleLine = true,
+                trailingIcon = {
+                    Button(
+                        contentPadding = PaddingValues(0.dp),
+                        shape = RoundedCornerShape(100),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent,
+                            contentColor = MaterialTheme.colorScheme.primary),
+                        onClick = toggleDialog
+                    ){
+                        Icon(Icons.Rounded.Settings, contentDescription = "Localized description")
+                    }
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        toggleDialog()
+                    }
+                ),
             )
             Text(text = "OR", style = MaterialTheme.typography.labelLarge)
-            Button(onClick = { /*TODO*/ }) {
+            Button(
+                onClick = {  }
+            ) {
                 Text(text = "Get Lucky", style = MaterialTheme.typography.labelLarge)
             }
         }
