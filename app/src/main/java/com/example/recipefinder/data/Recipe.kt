e7c02a95bd89a15dfcbf7e03b483f1f6da536ecf
Sebastian M <sebmueller1999@gmail.com>
2023-02-15 17:13:42 +0100

    added all values to recipe model

--- app/src/main/java/com/example/recipefinder/data/Recipe.kt
+++ app/src/main/java/com/example/recipefinder/data/Recipe.kt
@@ -1,9 +1,61 @@
 package com.example.recipefinder.data
 
+import androidx.compose.ui.Modifier
+
 data class RecipeModel (
-    val vegetarian: Boolean,
+   val vegetarian: Boolean,
     val title: String,
     val id: Int,
+    val vegan: Boolean,
+    val preparationMinutes: Int,
+    val cookingMinutes: Int,
+    val healthScore: Int,
+    val creditsText: String,
+    val sourceName: String,
+    val extendedIngredients: List<Ingredient>,
+    val readyInMinutes: Int,
+    val servings: Int,
+    val sourceUrl: String,
+    val imageType: String,
+    val summary: String,
+    val cuisines: List<String>,
+    val instructions: String,
+    val analyzedInstructions: List<Instruction>,
+
+
+
+
+
+)
+
+data class Ingredient(
+    val id: Int,
+    val image: String,
+    val name: String,
+    val nameClean: String,
+    val measures: Measures,
+)
+
+data class Measures(
+    val metric: Measure,
+)
+
+data class Measure(
+    val amount: Double,
+    val unitShort: String,
+)
+
+data class Instruction(
+    val number: Int,
+    val steps: List<Step>,
+
+
+)
+
+data class Step(
+    val number: Int,
+    val step: String,
+    val ingredients: List<Ingredient>,
 
 
 )
