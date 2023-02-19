adf29acaef18ed629811360d7a64e11edf68d029
Sebastian M <sebmueller1999@gmail.com>
2023-02-19 01:57:00 +0100

    added geolocation

--- app/src/main/java/com/example/recipefinder/MainActivity.kt
+++ app/src/main/java/com/example/recipefinder/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.recipefinder
 
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.res.Configuration
+import android.location.Geocoder
+import android.location.LocationRequest
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.collectAsState
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.MutableLiveData
@@ -14,13 +21,22 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.recipefinder.data.*
 import com.example.recipefinder.ui.RecipefinderApp
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import retrofit2.Call
+import retrofit2.Response
+import java.util.*
 
 class MainActivity : ComponentActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val themeViewModel: ThemeViewModel by viewModels()
     private lateinit var dataStoreUtil: DataStoreUtil
     private lateinit var recipeViewModel: RecipeViewModel
 
+    @RequiresApi(Build.VERSION_CODES.S)
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         super.onCreate(savedInstanceState)
         dataStoreUtil = DataStoreUtil(applicationContext)
 
@@ -32,6 +48,38 @@ class MainActivity : ComponentActivity() {
         recipeViewModel = ViewModelProvider(this).get(RecipeViewModel::class.java)
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    fusedLocationClient.getCurrentLocation(LocationRequest.QUALITY_HIGH_ACCURACY, null).addOnSuccessListener {
+                        Log.e("LocationLogDebug", it.toString())
+                        val lat = it.latitude
+                        val lng = it.longitude
+                        val gcd = Geocoder(this, Locale.getDefault())
+                        val addresses = gcd.getFromLocation(lat, lng, 1)
+
+                        if (addresses!!.size > 0) {
+                            val countryName = addresses!![0].countryName
+                            Log.e("LocationLogDebug", countryName)
+                        }
+                    }
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // Only approximate location access granted.
+                } else -> {
+                // No location access granted.
+            }
+            }
+        }
+
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
+
+
 
         setContent {
             val theme = dataStoreUtil.getTheme(systemTheme).collectAsState(initial = systemTheme)
